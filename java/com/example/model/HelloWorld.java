4696e73cb8a784d5d49ac87ed3ff01a7222f93d9
David Young-Chan Kay <davidykay@gmail.com>
2013-06-24 16:50:45 -0400

    trying to add tests.

---
+++ java/com/example/model/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.example.model;
+
+public class HelloWorld
+{
+
+   private String name = "";
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public String getMessage()
+   {
+      if (name == "")
+      {
+         return "Hello!";
+      }
+      else
+      {
+         return "Hello " + name + "!";
+      }
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+}
---
+++ java_test/com/example/model/MyFirstTest.java
@@ -0,0 +1,33 @@
+package com.example.activity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MyFirstTest {
+
+   private HelloWorld h;
+
+   @Before
+   public void setUp() throws Exception
+   {
+      h = new HelloWorld();
+   }
+
+   @Test
+   public void testHelloEmpty()
+   {
+      assertEquals(h.getName(),"");
+      assertEquals(h.getMessage(),"Hello!");
+   }
+
+   @Test
+   public void testHelloWorld()
+   {
+      h.setName("World");
+      assertEquals(h.getName(),"World");
+      assertEquals(h.getMessage(),"Hello World!");
+   }
+}
